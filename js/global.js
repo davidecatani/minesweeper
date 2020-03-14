cdbc16a2165efb6c14ad172088e7966911047e3a
davidecatani <davide.catani@ndp-studio.com>
2020-03-14 10:51:53 +0100

    Basic grid

--- js/global.js
+++ js/global.js
@@ -1,1 +1,21 @@
-console.log('here');
+const game = document.getElementById('game');
+const rows = 10;
+const cols = 10;
+const mines = 10;
+
+let wrapper = document.createElement('div');
+wrapper.classList.add('mine-wrapper');
+wrapper.style.width = cols * 30 + 'px';
+for (let r = 0; r < rows; r++) {
+    let row = document.createElement('div');
+    row.classList.add('mine-row');
+    for (let c = 0; c < cols; c++) {
+        let col = document.createElement('div');
+        col.classList.add('mine-cell')
+        col.appendChild(document.createTextNode('1'));
+        row.appendChild(col);
+    }
+    wrapper.appendChild(row);
+}
+
+game.appendChild(wrapper);
